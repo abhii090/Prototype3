fd4c0d1bc9e4db9cbb7cf43bd623bc0cb5fca211
Abhishek Bedsur <abhishek.bedsur@indivillage.co>
2025-10-30 12:07:41 +0530

    changes the playe and backgorund

--- Assets/Scenes/scripts/playercontroller.cs
+++ Assets/Scenes/scripts/playercontroller.cs
@@ -1,24 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpforce;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-     
-    } 
+        Physics.gravity *= gravityModifier;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false;
         }
     }
+      private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
 }
+
+
---
+++ Assets/Scenes/scripts/spawnmanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanger : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatrate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
