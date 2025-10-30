b21e9fed74b3713bf5c677a142bcada2bb0136b8
Abhishek Bedsur <abhishek.bedsur@indivillage.co>
2025-10-30 17:41:18 +0530

    changes

--- Assets/Scenes/scripts/playercontroller.cs
+++ Assets/Scenes/scripts/playercontroller.cs
@@ -1,25 +1,21 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float jumpforce;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
-    // Start is called before the first frame update
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
@@ -28,18 +24,17 @@ public class NewBehaviourScript : MonoBehaviour
             isOnGround = false;
         }
     }
-      private void OnCollisionEnter(Collision collision)
+
+    private void OnCollisionEnter(Collision collision)
     {
-       if(collision .gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-
-        }else if(collision.gameObject.CompareTag("obstacles"))
+        }
+        else if (collision.gameObject.CompareTag("obstacles"))
         {
             gameOver = true;
-            Debug.Log("game over!");
+            Debug.Log("Game Over!");
         }
     }
 }
-
-
--- Assets/Scenes/scripts/spawnmanger.cs
+++ Assets/Scenes/scripts/spawnmanger.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class spawnmanger : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    private PlayerController playerControllerScript;
 
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatrate = 2;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatrate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        
+
+    }
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
